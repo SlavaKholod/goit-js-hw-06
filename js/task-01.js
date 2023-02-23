ec4842ebd55ad971d9218a8b845bbad744511606
SlavaKholod <kholod.v.job@gmail.com>
2023-02-23 22:45:12 +0200

    task #1 add

--- js/task-01.js
+++ js/task-01.js
@@ -1,16 +1,20 @@
 "use strict";
 
 const categories = document.querySelector("#categories");
-const item = document.querySelector(".item");
-
-// const findNumOfCategories = () => {
-//   const num = categories.children.length;
-//   console.log(`Numbers of categories: ${num}`);
-// };
+const items = document.querySelectorAll(".item");
+const headers = document.querySelectorAll(".item > h2");
 
 const findChildren = () => {
   const num = categories.children.length;
   console.log(`Numbers of categories: ${num}`);
+
+  const findHeaderAndChildren = (item) => {
+    const headerOfItem = item.querySelector("h2");
+    const listOfItem = item.querySelector("ul");
+    console.log(`Category: ${headerOfItem.textContent}`);
+    console.log(`Elements: ${listOfItem.children.length}`);
+  };
+  items.forEach((item) => findHeaderAndChildren(item));
 };
 
 findChildren();
