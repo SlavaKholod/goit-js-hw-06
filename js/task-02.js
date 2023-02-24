300a19ef30c119e40f709587751bf986c582c1d6
SlavaKholod <kholod.v.job@gmail.com>
2023-02-24 10:37:19 +0200

    task #2 add

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsListEl = document.querySelector("#ingredients");
+
+const addItemsToList = ingredients.map((element) => {
+    const addItem = document.createElement("li");
+    addItem.textContent = element;
+    addItem.classList.add("item");
+    return addItem;
+  });
+
+ingredientsListEl.append(...addItemsToList);
