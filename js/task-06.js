0a51085d86b8497447342be720b038bbc7bceea7
SlavaKholod <kholod.v.job@gmail.com>
2023-02-26 16:12:36 +0200

    task #6 change

--- js/task-06.js
+++ js/task-06.js
@@ -3,16 +3,16 @@ inputEl.addEventListener("blur", inputValidation);
 
 function inputValidation() {
   if (
-    String(inputEl.value).length < inputEl.dataset.length &&
-    String(inputEl.value).length >= 1
+    String(inputEl.value).length >= 1 &&
+    String(inputEl.value).length != inputEl.dataset.length
   ) {
     inputEl.classList.remove("valid");
     inputEl.classList.add("invalid");
 
-  } else if (String(inputEl.value).length >= inputEl.dataset.length) {
+  } else if (String(inputEl.value).length == inputEl.dataset.length) {
     inputEl.classList.remove("invalid");
     inputEl.classList.add("valid");
-    
+
   } else if (inputEl.value === "") {
     inputEl.classList.remove("valid");
     inputEl.classList.remove("invalid");
