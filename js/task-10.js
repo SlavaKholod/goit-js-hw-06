c3329947eb92c549ae939102c5d31bb96fd85df8
SlavaKholod <kholod.v.job@gmail.com>
2023-02-27 16:17:19 +0200

    task #10 add

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,30 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const amountEl = document.querySelector("input[type='number']");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const containerEl = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  amount = amountEl.value;
+  let marcupTemplate = [];
+  for (let i = 0; i < amount; i++) {
+    const marcupEl = document.createElement("div");
+    let calculation = 30 + i * 10;
+    marcupEl.style.width = `${calculation}px`;
+    marcupEl.style.height = `${calculation}px`;
+    marcupEl.style.backgroundColor = getRandomHexColor();
+    marcupTemplate.push(marcupEl);
+  }
+  containerEl.append(...marcupTemplate);
+}
+
+function destroyBoxes() {
+  containerEl.innerHTML = "";
+  amountEl.value = "";
+}
